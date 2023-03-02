0d8e8d4db5cebd9909884a829e0fe270fecfda56
Jakub <jakubgradzewicz1309@gmail.com>
2023-03-02 11:39:07 +0100

    first endpoint, simple error handling

--- api/server.cjs
+++ api/server.cjs
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+var map = require("./story/map.json");
+
 app.get("/api/v1/test", (req, res) => {
-  res.send({ hello: "world" });
+  res.send({ status: "works" });
+});
+
+app.get("/api/v1/:location", (req, res, next) => {
+  let location = req.params.location;
+
+  if (!Object.keys(map).includes(location)) {
+    res.status(404).send(`Error! Location {${location}} does not exist!`);
+  }
+
+  res.send(map[location]);
 });
 
 app.listen(port, () => {
